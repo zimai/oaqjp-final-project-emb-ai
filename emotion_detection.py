006d52e22eb6c18a715825343d8b12f61bf6fa98
zimai <carwash-ashy-why@duck.com>
2025-04-24 17:36:17 -0400

    Initial commit

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, headers=headers, json=input_json)
+    return response.text
